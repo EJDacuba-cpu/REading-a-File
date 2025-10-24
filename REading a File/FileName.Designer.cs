37c89b5714fcb39fefc9a75c6dfb57ac6003e255
EJDACUBA-cpu <jhoshcunanan@gmail.com>
2025-10-24 17:37:04 +0800

    change the design

--- REading a File/FileName.Designer.cs
+++ REading a File/FileName.Designer.cs
@@ -37,7 +37,9 @@
             // btnOkay
             // 
             this.btnOkay.BackColor = System.Drawing.Color.Red;
-            this.btnOkay.Location = new System.Drawing.Point(333, 370);
+            this.btnOkay.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOkay.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnOkay.Location = new System.Drawing.Point(329, 251);
             this.btnOkay.Name = "btnOkay";
             this.btnOkay.Size = new System.Drawing.Size(118, 39);
             this.btnOkay.TabIndex = 7;
@@ -50,7 +52,7 @@
             this.txtFileName.Location = new System.Drawing.Point(136, 180);
             this.txtFileName.Multiline = true;
             this.txtFileName.Name = "txtFileName";
-            this.txtFileName.Size = new System.Drawing.Size(528, 161);
+            this.txtFileName.Size = new System.Drawing.Size(528, 34);
             this.txtFileName.TabIndex = 6;
             this.txtFileName.TextChanged += new System.EventHandler(this.txtFileName_TextChanged);
             // 
@@ -58,20 +60,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(146, 140);
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(130, 146);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(203, 37);
+            this.label1.Size = new System.Drawing.Size(216, 31);
             this.label1.TabIndex = 5;
             this.label1.Text = "Enter FileName";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtInput
             // 
-            this.txtInput.Location = new System.Drawing.Point(136, 42);
+            this.txtInput.Location = new System.Drawing.Point(136, 53);
             this.txtInput.Multiline = true;
             this.txtInput.Name = "txtInput";
-            this.txtInput.Size = new System.Drawing.Size(528, 84);
+            this.txtInput.Size = new System.Drawing.Size(528, 64);
             this.txtInput.TabIndex = 4;
             this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
             // 
